a5940072be33f0b276355d3d4642d0dc696f5e52
leeyc924 <leeyc924@naver.com>
2023-12-05 01:44:19 +0900

    Add tier api

--- src/router/tier.ts
+++ src/router/tier.ts
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import asyncify from 'express-asyncify';
 import userTierJson from '../db/userTier.json';
+import { sqlToDB } from '@db';
 
 const router = asyncify(Router());
 
 router.get('/list', async (req, res) => {
-  try {
-    const tierList = userTierJson;
-    res.json({ tierList });
-  } catch (error) {
-    res.status(500).json(error);
-  }
+  const sql = `
+    SELECT users.name, users.address, users.gender, users.age, users_tier.tier, users_tier.id
+    FROM users
+    JOIN users_tier ON users.id = users_tier.user_id
+    ORDER BY CASE WHEN users_tier.tier = 0 THEN 1 ELSE 0 END, users_tier.tier ASC, users.name ASC;
+  `;
+  const userList = await sqlToDB(sql);
+  res.json({ userList: userList.rows });
 });
 
 export default router;
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import asyncify from 'express-asyncify';
-import { sqlToDB } from '@db';
+import { commit, getTransaction, rollback, sqlExecMultipleRows, sqlExecSingleRow, sqlToDB } from '@db';
 import { CustomError, parseToNumber } from '@utils';
 import dayjs from 'dayjs';
 
@@ -24,17 +24,29 @@ router.get('/detail', async (req, res) => {
 });
 
 router.post('/add', async (req, res) => {
-  const name = req.body['name'];
-  const gender = req.body['gender'];
-  const address = req.body['address'];
-  const age = req.body['age'];
-  const join_dt = req.body['join_dt'];
+  const client = await getTransaction();
+  try {
+    const name = req.body['name'];
+    const gender = req.body['gender'];
+    const address = req.body['address'];
+    const age = req.body['age'];
+    const join_dt = req.body['join_dt'];
 
-  const sql = `INSERT INTO 
+    const sql = `
+      INSERT INTO 
       users (name, gender, age, address, join_dt)
-      values ('${name}','${gender}','${age}', '${address}', '${join_dt}');`;
-  await sqlToDB(sql);
-  res.status(200).json();
+      VALUES ('${name}','${gender}','${age}', '${address}', '${join_dt}');
+
+      INSERT INTO users_tier (user_id, tier)
+      VALUES (CURRVAL('users_id_seq'), 0);
+    `;
+    await sqlExecSingleRow(client, sql);
+    await commit(client);
+    res.status(200).json();
+  } catch (error) {
+    await rollback(client);
+    throw error;
+  }
 });
 
 router.post('/edit', async (req, res) => {
@@ -59,10 +71,20 @@ router.post('/edit', async (req, res) => {
 });
 
 router.post('/remove', async (req, res) => {
-  const id = parseToNumber(req.body['id']);
-  const sql = `DELETE FROM users WHERE "id" = ${id};`;
-  await sqlToDB(sql);
-  res.status(200).json();
+  const client = await getTransaction();
+  try {
+    const id = parseToNumber(req.body['id']);
+    const sql = `
+    DELETE FROM users WHERE "id" = ${id};
+    DELETE FROM users_tier WHERE "user_id" = ${id};
+  `;
+    await sqlExecSingleRow(client, sql);
+    await commit(client);
+    res.status(200).json();
+  } catch (error) {
+    await rollback(client);
+    throw error;
+  }
 });
 
 export default router;
