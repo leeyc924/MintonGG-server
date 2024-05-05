8215c44cc214ea63b10717f5add85135de716736
leeyc924 <leeyc924@naver.com>
2024-05-05 14:01:05 +0900

    Add postgre module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from '@config/configuration';
+import { TypeOrmConfigService } from '@config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: this.configService.get('db.host'),
+      port: this.configService.get('db.port'),
+      username: this.configService.get('db.username'),
+      database: this.configService.get('db.database'),
+      password: this.configService.get('db.password'),
+      synchronize: this.configService.get('db.synchronize'),
+      entities: [],
+    };
+  }
+}
